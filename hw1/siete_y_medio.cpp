074056bd788231648ca776b91e31b4a105f331dd
Kevin Tran <tran.kevin53@yahoo.com>
2018-01-17 12:31:14 -0800

    Printout Process
    Added code to print logfile.txt

--- hw1/siete_y_medio.cpp
+++ hw1/siete_y_medio.cpp
@@ -23,35 +23,43 @@ int main() {
 	int dealer_loss(0);
 	int bet(0);
 	bool winner(false);
+	ofstream myfile;
 
 	//game will keep playing as long as player's money is above 0
 	while (Person.get_money() > 0) {
-		cout << "You have $" << Person.get_money() << ". ";
+		std::cout << "You have $" << Person.get_money() << ". ";
 		//internal check to make sure proper betting is occuring
 		while (bet == 0 || bet > Person.get_money()) {
-			cout << "Place your bet: ";
+			std::cout << "Place your bet: ";
 			cin >> bet;
 		}
 
-		cout << "Your cards:" << endl;
+		std::cout << "Your cards:" << endl;
 
 		//CODE TO RUN GAME GOES HERE
 
 		//if won: declare winner and add to Player's money and dealer's loss. Do inverse for loss
 		if (winner) {
 			Person.change_money(bet);
-			cout << "You won! You have won " << bet << " dollars!" << endl;
+			std::cout << "You won! You have won " << bet << " dollars!" << endl;
 			dealer_loss += bet;
 		}
 		else {
 			Person.change_money(-1 * bet);
-			cout << "You lost! You have lost " << bet << " dollars!" << endl;
+			std::cout << "You lost! You have lost " << bet << " dollars!" << endl;
 			dealer_loss -= bet;
 		}
-		cout << "You now have " << Person.get_money() << " dollars to play with!" << endl;
+		std::cout << "You now have " << Person.get_money() << " dollars to play with!" << endl;
 
 		//set bet back to 0 to restart game loop
 		bet = 0;
+
+		//CODE TO WRITE TO LOG FILE AFTER EACH GAME
+		//NEED TO INSERT CLASS FUNCTIONS IN GAPS
+		myfile.open("gamelog.txt");
+		myfile << "Write test";
+		myfile.close();
+
 	}
 
 	return 0;
